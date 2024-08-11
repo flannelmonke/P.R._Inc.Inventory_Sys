9ab503f316db23cb00bfb3d9ca6a4052b4344a02
flannelmonke <khalilanaji@gmail.com>
2024-08-11 17:51:59 -0400

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['JetBrains Mono', 'sans-serif']
+      },
+    },
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+}
