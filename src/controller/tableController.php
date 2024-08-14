14c4fabaac58c5c4daabc6dc14b22f9a11ad9f75
flannelmonke <khalilanaji@gmail.com>
2024-08-14 14:00:38 -0400

    chore: Update tableController.php to display data in table

--- src/controller/tableController.php
+++ src/controller/tableController.php
@@ -1,5 +1,9 @@
 <?php
 
-    
+require_once '../database/dbAccess.php';
 
-?>
+$data = loadData();
+
+for ($x = 0; $x < count($data); $x++) {
+    echo "<tr><th>" . $x . "</th>" . $data[$x] . "<th>" . $x . "</th></tr>";
+}
